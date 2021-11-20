ccf8fc6596921719e7265adc66fd55b70e752725
Evan Gray <battledingo@gmail.com>
2021-11-19 19:45:36 -0500

    add logo, update structure

--- src/__tests__/validate.js
+++ src/__tests__/validate.js
@@ -10,14 +10,26 @@ test("markets.json is valid", () => {
 
   const valid = validate(data);
 
+  if (!valid) console.log(validate.errors);
+
   expect(valid).toBe(true);
+});
+test("all tokens are keys in tokens", () => {
+  const data = require("../markets.json");
+  for (const sourceChain in data.tokenMarkets) {
+    for (const targetChain in data.tokenMarkets[sourceChain]) {
+      for (const address in data.tokenMarkets[sourceChain][targetChain]) {
+        expect(data.tokens[sourceChain][address]).toBeDefined();
+      }
+    }
+  }
 });
 test("all markets are keys in markets", () => {
   const data = require("../markets.json");
-  for (const sourceChain in data.marketsBySource) {
-    for (const targetChain in data.marketsBySource[sourceChain]) {
-      for (const address in data.marketsBySource[sourceChain][targetChain]) {
-        data.marketsBySource[sourceChain][targetChain][address].markets.forEach(
+  for (const sourceChain in data.tokenMarkets) {
+    for (const targetChain in data.tokenMarkets[sourceChain]) {
+      for (const address in data.tokenMarkets[sourceChain][targetChain]) {
+        data.tokenMarkets[sourceChain][targetChain][address].markets.forEach(
           (market) => {
             expect(data.markets[market]).toBeDefined();
           }
