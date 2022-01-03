c8b863555964c4c2df6f421da95d790b6bbb284e
Keone Hon <keone.hon@gmail.com>
2022-01-03 10:56:09 -0600

    add traderjoe to market_urls.py

--- src/config/market_urls.py
+++ src/config/market_urls.py
@@ -1,4 +1,27 @@
 MARKETS = {
+  #################
+  # 1. sol markets
+  #################
+  "aldrin": {
+    "name": "Aldrin",
+    "link": "https://dex.aldrin.com/pools"
+  },
+  "almond": {
+    "name": "Almond",
+    "link": "https://almond.so/"
+  },
+  "apricot": {
+    "name": "Apricot",
+    "link": "https://app.apricot.one/"
+  },
+  "atrix": {
+    "name": "Atrix",
+    "link": "https://app.atrix.finance/"
+  },
+  "francium": {
+    "name": "Francium",
+    "link": "https://francium.io/app/invest/farm"
+  },
   "saber": {
     "name": "Saber",
     "link": "https://www.saber.so/"
@@ -11,34 +34,46 @@ MARKETS = {
     "name": "Jupiter",
     "link": "https://jup.ag/"
   },
-  "atrix": {
-    "name": "Atrix",
-    "link": "https://app.atrix.finance/"
+  "orca": {
+    "name": "Orca",
+    "link": "https://www.orca.so/"
+  },
+  "port": {
+    "name": "Port Finance",
+    "link": "https://mainnet.port.finance/#/markets"
   },
   "raydium": {
     "name": "Raydium",
     "link": "https://raydium.io/swap/"
   },
+  "sunny": {
+    "name": "Sunny",
+    "link": "https://app.sunny.ag/"
+  },
   "tulip": {
     "name": "Tulip",
     "link": "https://tulip.garden/"
   },
-  "almond": {
-    "name": "Almond",
-    "link": "https://almond.so/"
-  },
-  "orca": {
-    "name": "Orca",
-    "link": "https://www.orca.so/"
-  },
+
+  #################
+  # 2. eth markets
+  #################
   "curve": {
     "name": "Curve",
     "link": "https://curve.fi/"
+  },
+  "sushi": {
+    "name": "Sushi",
+    "link": "https://app.sushi.com/"
   },
   "uniswap": {
     "name": "Uniswap",
     "link": "https://app.uniswap.org/"
   },
+
+  ##################
+  # 3. terra markets
+  ##################
   "anchor": {
     "name": "Anchor",
     "link": "https://app.anchorprotocol.com/"
@@ -47,36 +82,28 @@ MARKETS = {
     "name": "Loop Markets",
     "link": "https://dex.loop.markets/"
   },
-  "quickswap": {
-    "name": "Quickswap",
-    "link": "https://quickswap.exchange/"
-  },
-  "sushi": {
-    "name": "Sushi",
-    "link": "https://app.sushi.com/"
-  },
+
+  ##################
+  # 4. BSC markets
+  ##################
   "pancakeswap": {
     "name": "Pancakeswap",
     "link": "https://pancakeswap.finance/swap"
   },
-  "sunny": {
-    "name": "Sunny",
-    "link": "https://app.sunny.ag/"
+
+  #####################
+  # 5. polygon markets
+  #####################
+  "quickswap": {
+    "name": "Quickswap",
+    "link": "https://quickswap.exchange/"
   },
-  "port": {
-    "name": "Port Finance",
-    "link": "https://mainnet.port.finance/#/markets"
+
+  #######################
+  # 6. avalanche markets
+  #######################
+  "traderjoe": {
+    "name": "Trader Joe",
+    "link": "https://traderjoexyz.com/#/trade"
   },
-  "francium": {
-    "name": "Francium",
-    "link": "https://francium.io/app/invest/farm"
-  },
-  "aldrin": {
-    "name": "Aldrin",
-    "link": "https://dex.aldrin.com/pools"
-  },
-  "apricot": {
-    "name": "Apricot",
-    "link": "https://app.apricot.one/"
-  }
 }
