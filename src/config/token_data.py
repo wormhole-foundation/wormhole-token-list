3dbd2786186d6775c4c46139e7f2112fccd97dbe
Bharath Vedartham <vedabharath12345@gmail.com>
2022-03-14 14:54:47 +0530

    Remove spacing

--- src/config/token_data.py
+++ src/config/token_data.py
@@ -1185,7 +1185,7 @@ TOKENS = {
       },
       "sourceAddress": "0x30D20208d987713f46DFD34EF128Bb16C404D10f",
       "coingeckoId": "stader",
-      "logo": " https://raw.githubusercontent.com/stader-labs/assets/main/eth/SD.png"
+      "logo": "https://raw.githubusercontent.com/stader-labs/assets/main/eth/SD.png"
     },
   },
 
