3dfd5b88f41ab72e05ad43e65fef1ddba7b32a17
Sebastian Scatularo <sebastian.scatularo@xlabs.xyz>
2023-08-03 12:26:40 -0300

    add tBTC v2 for Base mainnet

--- src/config/token_data.py
+++ src/config/token_data.py
@@ -1582,6 +1582,10 @@ TOKENS = {
         "optimism": {
           "address": "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40",
           "decimals": 18
+        },
+        "base": {
+          "address": "0x236aa50979D5f3De3Bd1Eeb40E81137F22ab794b",
+          "decimals": 18
         }
       },
       "sourceDecimals": 18
@@ -2460,6 +2464,10 @@ TOKENS = {
         "optimism": {
           "address": "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40",
           "decimals": 18
+        },
+        "base": {
+          "address": "0x236aa50979D5f3De3Bd1Eeb40E81137F22ab794b",
+          "decimals": 18
         }
       },
       "sourceDecimals": 18
@@ -2872,6 +2880,10 @@ TOKENS = {
         "optimism": {
           "address": "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40",
           "decimals": 18
+        },
+        "base": {
+          "address": "0x236aa50979D5f3De3Bd1Eeb40E81137F22ab794b",
+          "decimals": 18
         }
       },
       "sourceDecimals": 18
@@ -2904,6 +2916,10 @@ TOKENS = {
         "arbitrum": {
           "address": "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40",
           "decimals": 18
+        },
+        "base": {
+          "address": "0x236aa50979D5f3De3Bd1Eeb40E81137F22ab794b",
+          "decimals": 18
         }
       },
       "sourceDecimals": 18
@@ -2913,5 +2929,41 @@ TOKENS = {
   # 28. XPLA native
   #################
   "xpla": {
+  },
+  #################
+  # 30. Base native
+  #################
+  "base": {
+    "TBTC": {
+      "symbol": "TBTC",
+      "name": "Threshold Bitcoin",
+      "sourceAddress": "0x236aa50979D5f3De3Bd1Eeb40E81137F22ab794b",
+      "coingeckoId": "tbtc",
+      "logo": "https://assets.coingecko.com/coins/images/11224/small/0x18084fba666a33d37592fa2633fd49a74dd93a88.png?1674474504",
+      "markets": {
+        "eth": ['threshold'],
+        "matic": ['threshold'],
+        "arbitrum": ['threshold']
+      },
+      "destinations": {
+        "eth": {
+          "address": "0x18084fbA666a33d37592fA2633fD49a74DD93a88",
+          "decimals": 18
+        },
+        "matic": {
+          "address": "0x236aa50979D5f3De3Bd1Eeb40E81137F22ab794b",
+          "decimals": 18
+        },
+        "arbitrum": {
+          "address": "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40",
+          "decimals": 18
+        },
+        "optimism": {
+          "address": "0x6c84a8f1c29108F47a79964b5Fe888D4f4D0dE40",
+          "decimals": 18
+        }
+      },
+      "sourceDecimals": 18
+    }
   }
 }
